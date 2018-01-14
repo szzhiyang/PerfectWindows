de68b545a3a368634a9647f224fea77a620e3641
Tom Zhu <szzhiyang@outlook.com>
2018-01-14 19:14:57 +0800

    添加了 Hosts Editor 。

---
+++ PerfectWindows/Hosts Editor.bat
@@ -0,0 +1,13 @@
+@echo off
+
+md "%systemroot%\checkadmin"
+if exist "%systemroot%\checkadmin" (
+rd /s /q "%systemroot%\checkadmin"
+goto main) else (
+exit
+)
+
+:main
+notepad %systemroot%\system32\drivers\etc\hosts
+flushdns
+exit
